1687a79fd3fd60e4f900f5eef08f6e2f664949eb
KSnivy333 <david.parra.diaz@alumnojoyfe.iepgroup.es>
2025-12-10 19:00:52 +0100

    testeo

---
+++ src/components/pokemonBoxComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <RouterLink class="pokemon-box-item" :to="to">
+    <div class="pokemon-box-img-container">
+      <img :src="img" :alt="`Imagen de ${name}`" />
+    </div>
+
+    <span>{{ number + " " + name }}</span>
+  </RouterLink>
+</template>
+
+<script setup>
+const props = defineProps({
+  img: String,
+  name: String,
+  number: [Number, String],
+  to: {
+    type: String,
+    required: true
+  }
+});
+</script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
-// src/main.js
-import { createApp } from 'vue';
-import App from './App.vue';
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
 
-// ... (posiblemente importaciones de router, pinia, etc.)
-
-createApp(App)
-  // .use(router) // Si tienes Vue Router
-  .mount('#app'); // Monta App.vue en el div con id="app"
+createApp(App).use(router).mount('#app')
